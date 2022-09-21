a78dd0f8df7b9942adda4ecc1c83cf5febcad6a4
m09 <142691+m09@users.noreply.github.com>
2022-09-21 09:40:24 +0200

    Add an entrypoint that says Hello World
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

---
+++ ghcli/cli.py
@@ -0,0 +1,6 @@
+"""Command Line Interface (CLI) to interact with GitHub issues."""
+
+
+def main() -> None:
+    """Parse CLI arguments and call the corresponding parts of GHCLI."""
+    print("Hello World")
