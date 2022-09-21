788cf0a7faee95ffa6af56536e249e682da5e0b1
m09 <142691+m09@users.noreply.github.com>
2022-09-21 10:24:13 +0200

    Implement create_issue and adapt the parser
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- ghcli/api.py
+++ ghcli/api.py
@@ -44,3 +44,34 @@ def list_issues(owner: str, repo: str) -> typing.List[typing.Tuple[str, str, str
     # Nous filtrons enfin cette structure pour renvoyer seulement 3 valeurs de chaque
     # dictionnaire : le titre, le corps et l'url des issues.
     return [(issue["title"], issue["body"], issue["html_url"]) for issue in data]
+
+
+def create_issue(
+    owner: str, repo: str, title: str, body: str
+) -> typing.Tuple[str, str, str]:
+    """Retrieve issues of a GitHub repository."""
+    # Appel à l'API GitHub comme détaillé ici :
+    # https://docs.github.com/en/rest/issues/issues#create-an-issue
+    # Cet appel permet d'obtenir un objet réponse de la bibliothèque requests :
+    # https://requests.readthedocs.io/en/latest/user/quickstart/#response-content
+    response = requests.post(
+        f"https://api.github.com/repos/{owner}/{repo}/issues",
+        json=dict(title=title, body=body),
+        headers=dict(
+            Accept="application/vnd.github.v3+json",
+            Authorization=f"token {_get_token()}",
+        ),
+    )
+
+    # Vérifions d'abord qu'il n'y a pas eu de problème. Si la requête a renvoyé un code
+    # d'erreur, une exception sera levée ici
+    response.raise_for_status()
+
+    # Nous pouvons maintenant récupérer le contenu de la réponse avec la méthode json.
+    # Celle-ci va nous renvoyer une structure de données Python qui correspond au json
+    # de la réponse. Ici, ce sera un dictionnaire
+    issue = response.json()
+
+    # Nous filtrons enfin cette structure pour renvoyer seulement 3 valeurs : le titre,
+    # le corps et l'url.
+    return issue["title"], issue["body"], issue["html_url"]
--- ghcli/cli.py
+++ ghcli/cli.py
@@ -1,15 +1,10 @@
 """Command Line Interface (CLI) to interact with GitHub issues."""
 import argparse
 
-from .api import list_issues
+from .api import create_issue, list_issues
 
 
-def _create_parser() -> argparse.ArgumentParser:
-    # Création du parseur d'arguments
-    parser = argparse.ArgumentParser(
-        description="GHCLI - CLI to list and create GitHub issues."
-    )
-
+def _add_repo_args(parser: argparse.ArgumentParser) -> None:
     # Ajout de l'option owner avec une valeur par défaut qui pointe vers le dépôt git
     # du tp. L'autre option aurait été d'ajouter ici un argument (même syntaxe mais sans
     # les --). Il aurait alors été obligatoire de fournir une valeur pour appeler GHCLI
@@ -21,6 +16,31 @@ def _create_parser() -> argparse.ArgumentParser:
     # du tp. L'autre option aurait été d'ajouter ici un argument (même syntaxe mais sans
     # les --). Il aurait alors été obligatoire de fournir une valeur pour appeler GHCLI
     parser.add_argument("--repo", default="tp-ghcli", help="GitHub repository")
+
+
+def _create_parser() -> argparse.ArgumentParser:
+    # Création du parseur d'arguments
+    parser = argparse.ArgumentParser(
+        description="GHCLI - CLI to list and create GitHub issues."
+    )
+
+    # Création d'un objet subparsers. Celui-ci nous permettra de créer un parseur par
+    # commande : un pour la commande list, un pour la commande create
+    subparsers = parser.add_subparsers(help="Commands", dest="command", required=True)
+
+    # Création du sous-parseur pour la commande list
+    parser_list = subparsers.add_parser("list")
+    # Ajout des arguments pour identifier le dépôt GitHub à cibler
+    _add_repo_args(parser_list)
+
+    # Création du sous-parseur pour la commande create
+    parser_create = subparsers.add_parser("create")
+    # Ajout des arguments pour identifier le dépôt GitHub à cibler
+    _add_repo_args(parser_create)
+    # Ajout des arguments qui permettent de spécifier le contenu de l'issue à créer
+    parser_create.add_argument("title", help="Title of the GitHub issue")
+    parser_create.add_argument("body", help="Body of the GitHub issue")
+
     return parser
 
 
@@ -33,6 +53,17 @@ def main() -> None:
     args = parser.parse_args()
 
     # Utilisation de ces arguments
-    issues = list_issues(owner=args.owner, repo=args.repo)
-    for title, body, url in issues:
+
+    # D'abord, vérifions quelle commande a été appelée
+    if args.command == "list":
+        # Ensuite nous pouvons utiliser les arguments récupérer pour appeler la fonction
+        # correspondante
+        issues = list_issues(owner=args.owner, repo=args.repo)
+        for title, body, url in issues:
+            print(f"{url} - {title} - {body}")
+    else:
+        # Et de même pour la commande create
+        title, body, url = create_issue(
+            owner=args.owner, repo=args.repo, title=args.title, body=args.body
+        )
         print(f"{url} - {title} - {body}")
