321a5ea2020ce0e0b47dc1bb1b2ec0cc3639c7f9
m09 <142691+m09@users.noreply.github.com>
2022-09-21 10:04:29 +0200

    Retrieve GitHub token from the GHCLI_TOKEN env variable
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- ghcli/api.py
+++ ghcli/api.py
@@ -1,6 +1,22 @@
 """Interact with the GitHub API."""
+import os
+import sys
+
+
+def _get_token() -> str:
+    try:
+        return os.environ["GHCLI_TOKEN"]
+    except KeyError:
+        print(
+            "Impossible de récupérer le token GitHub dans la variable d'environnement "
+            "GHCLI_TOKEN. Définissez cette variable et relancez le programme.",
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
 
 def list_issues(owner: str, repo: str) -> None:
     """Retrieve issues of a GitHub repository."""
     print(f"Arguments récupérés par list_issues : owner={owner}, repo={repo}")
+    token = _get_token()
+    print(f"Token récupéré depuis _get_token : {token}")
